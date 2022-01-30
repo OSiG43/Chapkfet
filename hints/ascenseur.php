d2c2d7d19368b3dbde5ea0ce72c905a0f3ee22c7
peufgeul <montoyasamuel2000@gmail.com>
2022-01-31 00:29:00 +0100

    template questions
    creation d'un template pour les questions, plus changement du décompte des points

--- hints/ascenseur.php
+++ hints/ascenseur.php
@@ -3,16 +3,16 @@
 
   //On vérifie que la personne est connectée, sinon on la redirige vers la page de connection
   if(!isset($_SESSION['bucque'])){
-    header("Location: ../login.php?p=hints/abscences"); //Ne pas oublié de changer choucroute pour le nom du fichier.
+    header("Location: ../login.php?p=hints/ascenseur"); //Ne pas oublié de changer choucroute pour le nom du fichier.
   }
   
   require("../utils/user_utils.php"); // on importe user utils pour la suite.
 
   //variables à modifier
-  $hintNumber = 20;
-  $bonnesReponses = array('esteban','non');
-  $requestDay=6;
-  $question="Qui à le plus d'abscences en cours ?";
+  $hintNumber = 1;
+  $bonnesReponses = array('esteban');
+  $requestDay=1;
+  $question="Qui a déjà dormi dans l'ascenseur ?";
 
   $neededLevels = range(1,$requestDay)
   $requestMonth=2;
