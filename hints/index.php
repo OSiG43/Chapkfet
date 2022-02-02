9e648138dac7779cd003b96cbeacea80819282b6
OSiG43 <simon.galand43@gmail.com>
2022-02-02 22:15:15 +0100

    Empecher les gens de tricher !!

---
+++ hints/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Non non non</title>
+	<style type="text/css">
+		.noImg {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    
+		} 
+	</style>
+</head>
+<body>
+	<img class="noImg" src="https://media2.giphy.com/media/RiuNsymGB8ilATGiDu/giphy.gif?cid=790b7611f9b686a4849b55027a5d45cc70955b4b43bb22f7&rid=giphy.gif&ct=g">
+</body>
+</html>
