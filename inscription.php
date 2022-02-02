d3ef3531f33328d4e36b42e2f18e3f736967de23
OSiG43 <simon.galand43@gmail.com>
2022-02-02 20:15:56 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -81,6 +81,15 @@
 
         
         <form method="post">
+            <p>
+                Bienvenu(e) dans le jeux de la semaine motiv's de la ChapK'fet.<br>
+L'objectif de ce jeux est d'obtenir le plus de points pour pouvoir remporter le graal.<br>
+Comment remporter des points ? <br>
+Chaque jour de la semaine, un nouveau niveau sera déblocable. Pour le débloquer, il faudra trouver dans la rez le QRcode de déblocage.<br>
+Une fois ce QRcode trouvé, vous débloquez le niveau et vous devez maintenant trouver les QRcodes défis. Ces derniers vous enverons vers des défis sous forme de QCM. Chaque bonne réponse vaut 1 point.<br>
+Chaque soir, vous pourrez vous rendre compte des points que vous aurez gagné dans la journée et à la fin de la semaine, celui ou celle qui aura obtenu le plus de points remportera ... .<br>
+A vos téléphones ! Et c'est parti !
+            </p>
             <h1>Inscription</h1>
             <div class="container">
                 <label=><strong>Bucque</strong></label>
