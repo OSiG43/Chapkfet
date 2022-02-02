8bf33522c9f58912ac92be021798f1221770052f
peufgeul <montoyasamuel2000@gmail.com>
2022-02-03 00:03:11 +0100

    questions jeudi

---
+++ hints/fillot.php
@@ -0,0 +1,20 @@
+<?php
+  session_start();
+
+  //On vérifie que la personne est connectée, sinon on la redirige vers la page de connection
+  if(!isset($_SESSION['bucque'])){
+    header("Location: ../login.php?p=hints/descente"); //Ne pas oublié de changer choucroute pour le nom du fichier.
+  }
+  
+  require("../utils/user_utils.php"); // on importe user utils pour la suite.
+
+  //variables à modifier
+  $hintNumber = 8;
+  $bonnesReponses = array('lysandre','esteban','nono','eythan');
+  $requestDay=3;
+  $question="Qui est fillot de la DSKfet ?";
+
+  $neededLevels = range(1,$requestDay);
+  $requestMonth=2;
+  require('../hints/template.php');
+?>
